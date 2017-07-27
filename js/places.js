91420015b0419c663b9a461ab49c3a43ef6ccf24
joeylaguna <elagunaj@gmail.com>
2017-07-27 16:20:52 -0700

    remove google utility functions

--- js/places.js
+++ js/places.js
@@ -1,56 +1,7 @@
-var map;
-var service;
-var pyrmont = new google.maps.LatLng(38.5718873,-121.4819011);
-var infoWindow = new google.maps.InfoWindow();
-var markers = [];
-var places = [];
 var ratingSorted = false;
 var nameSorted = false;
 
-function initializeMap() {
-  //Get user location through browser
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 12
-  });
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      let pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      }
-      map.setCenter(pos);
-    });
-  }
-  service = new google.maps.places.PlacesService(map);
-}
-
-const addMarker = (place) => {
-  let marker = new google.maps.Marker({
-    position: place.geometry.location,
-    map: map
-  });
-  marker.addListener('click', () => {
-    infoWindow.setContent(place.name);
-    infoWindow.open(map, marker);
-  });
-
-  marker.addListener('mouseover', () => {
-    let targetElement = document.querySelector(`[data='${place.id}']`);
-    targetElement.classList.add('activeHover');
-    targetElement.classList.add('fadeIn');
-  });
-
-  marker.addListener('mouseout', () => {
-    let targetElement = document.querySelector(`[data='${place.id}']`);
-    targetElement.classList.remove('activeHover');
-  });
-  markers.push(marker);
-  places.push(place);
-}
-
 const toggleRatingIcon = (type) => {
-
   if (type === 'rating') {
     let icon = document.querySelector('.rating');
     ratingSorted ? icon.innerHTML = 'arrow_drop_down' : icon.innerHTML = 'arrow_drop_up';
@@ -60,27 +11,6 @@ const toggleRatingIcon = (type) => {
     let icon = document.querySelector('.name');
     ratingSorted ? icon.innerHTML = 'arrow_drop_down' : icon.innerHTML = 'arrow_drop_up';
   }
-
-}
-
-const setMapOnAll = (map) => {
-  for (let i = 0; i < markers.length; i++) {
-    markers[i].setMap(null);
-  }
-}
-
-const clearMarkers = () => {
-  setMapOnAll(map);
-}
-
-const deleteMarkers = () => {
-  clearMarkers();
-  markers = [];
-  places = [];
-}
-
-function createMarker(place) {
-  addMarker(place);
 }
 
 const clearTable = () => {
@@ -124,36 +54,6 @@ const buildList = (place) => {
   table.appendChild(listing);
 }
 
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    document.querySelector('.loader').classList.add('none');
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(place);
-    }
-    sortByRating(places);
-  }
-  document.querySelector('.loader').classList.add('none');
-  document.querySelector('.searchResults').classList.remove('none');
-  
-}
-
-const handleSearch = (searchField, searchForm) => {
-  deleteMarkers();
-  clearTable();
-  document.querySelector('.tableHead').classList.remove('none');
-  document.querySelector('.searchResults').classList.add('none');
-  let query = searchField.value;
-  let queryObj = {};
-  queryObj['location'] = map.center;
-  queryObj['radius'] = '500',
-  queryObj['query'] = query;
-  service.textSearch(queryObj, callback);
-
-  //Reset search field
-  searchForm.reset();
-}
-
 const sortByRating = () => {
   places = mergeSort(places, 'rating', ratingSorted);
   clearTable();
@@ -181,7 +81,6 @@ const sortByName = () => {
 
 window.onload = () => {
   initializeMap();
-
   //Add event listener to input field
   let searchForm = document.querySelector('.searchArea');
   let searchField = document.querySelector('.searchField');
